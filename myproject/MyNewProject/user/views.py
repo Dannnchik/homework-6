80c817659d46e9888bc0207d6f3af5101ce6a26a
Doni <daakmed8890@gmail.com>
2024-10-25 19:53:02 +0600

    Add files via upload

---
+++ myproject/MyNewProject/user/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('post_list')
+    return render(request, 'user/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
